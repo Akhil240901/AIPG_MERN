b04aa47df6d155150a3aacb70210f5a1619d6f2d
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2025-01-07 13:52:24 +0530

    navbar is ready

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  height: 100%;
+  overflow-y: scroll;
+`;
+export const CreatePost = () => {
+  return <Container>Create-post</Container>;
+};
